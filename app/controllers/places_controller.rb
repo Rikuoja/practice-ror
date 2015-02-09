10bb99e9fc8012b3ec75f38cf9f35cd172393e8b
Riku Oja <riku.oja@kapsi.fi>
2015-02-09 23:23:47 +0200

    Add show place & google map

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,8 +1,16 @@
 class PlacesController < ApplicationController
+  before_action :set_place, only: [:show]
+
   def index
   end
 
+  def show
+  end
+
   def search
+
+    #save the last search for showing the place
+    session[:last_city] = params[:city].downcase
 
     @places = BeermappingApi.places_in(params[:city])
     if @places.empty?
@@ -10,5 +18,14 @@ class PlacesController < ApplicationController
     else
       render :index
     end
+  end
+
+  private
+
+  def set_place
+
+    #find the index in cached array
+    place_index=Rails.cache.read(session[:last_city]).find_index{|place| place.send(:id)==params[:id]}
+    @place = Rails.cache.read(session[:last_city])[place_index]
   end
 end
